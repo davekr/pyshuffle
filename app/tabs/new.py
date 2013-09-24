df5b63781e0cef225275f53f36ec4ebb70a04376
davekr <david.krutky@seznam.cz>
2013-09-24 16:14:15 +0200

    Improved New tab

--- app/tabs/new.py
+++ app/tabs/new.py
@@ -1,46 +1,53 @@
 from PyQt4 import QtCore, QtGui
 
 from app.forms import ActionForm, ProjectForm, ContextForm
+from app.tabs.tab import Tab
 
-class New(object):
+class New(Tab):
 
-    def setup_new(self, app, mainWidget):
+    ICON = "new"
+    LABEL = "New"
         
-        newWidget=QtGui.QWidget(mainWidget.tab)
-        newLayout=QtGui.QVBoxLayout(newWidget)
+    def _setup_content(self):
+        selection = self._setup_selection()
+        forms = self._setup_forms()
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(selection, 0)
+        layout.addWidget(QtGui.QLabel("<hr />"), 0)
+        layout.addWidget(forms, 1)
+        self.setLayout(layout)
 
-        selectWidget=QtGui.QWidget(newWidget)
-        selectLayout=QtGui.QHBoxLayout(selectWidget)
-        selectLabel=QtGui.QLabel("Choose item to create: ",selectWidget)
-        pageComboBox = QtGui.QComboBox(selectWidget)
-        pageComboBox.addItem("New action")
-        pageComboBox.addItem("New project")
-        pageComboBox.addItem("New context")
-        selectLayout.addWidget(selectLabel, 0, QtCore.Qt.AlignRight)
-        selectLayout.addWidget(pageComboBox, 0, QtCore.Qt.AlignLeft)
-        selectLayout.addWidget(QtGui.QWidget(), 1)
+    def _setup_selection(self):
+        select_layout = QtGui.QHBoxLayout()
+        select_layout.addWidget(QtGui.QLabel("Choose item to create: "), 0, \
+                               QtCore.Qt.AlignRight)
+        combobox = self._setup_combobox()
+        select_layout.addWidget(combobox, 0, QtCore.Qt.AlignLeft)
+        select_layout.addWidget(QtGui.QWidget(), 1)
+        select_widget = QtGui.QWidget()
+        select_widget.setLayout(select_layout)
+        return select_widget 
 
-        stack=QtGui.QStackedWidget(newWidget)
-        
-        self.action = ActionForm(stack)
-        stack.addWidget(self.action)
-        self.project = ProjectForm(stack, app, mainWidget)
-        stack.addWidget(self.project)
-        context = ContextForm(stack, app, mainWidget)
+    def _setup_combobox(self):
+        combobox = QtGui.QComboBox()
+        combobox.addItem("New action")
+        combobox.addItem("New project")
+        combobox.addItem("New context")
+        self._combobox = combobox
+        return combobox
+
+    def _setup_forms(self):
+        stack = QtGui.QStackedWidget()
+        stack.addWidget(ActionForm())
+        stack.addWidget(ProjectForm())
+        context = ContextForm()
         stack.addWidget(context)
         stack.addWidget(context.chooseColor)
         stack.addWidget(context.chooseIcon)
-        
-        newLayout.addWidget(selectWidget, 0)
-        newLayout.addWidget(QtGui.QLabel("<hr />"), 0)
-        newLayout.addWidget(stack, 1)
-        
-        app.connect(pageComboBox, QtCore.SIGNAL("activated(int)"),stack, 
-                    QtCore.SLOT("setCurrentIndex(int)"))
-        
-        return newWidget
-    
-    def refresh_new(self):
-        self.action.refreshAction()
-        self.project.refreshProject()
+        self._stack = stack
+        return stack
+
+    def _connect_events(self):
+        self.connect(self._combobox, QtCore.SIGNAL("activated(int)"), self._stack, \
+                     QtCore.SLOT("setCurrentIndex(int)"))
             
